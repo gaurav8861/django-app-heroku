754117362aff3d289c7f4b7dbf198de2606bbe51
Gaurav Kumar <gkumar@cray.com>
2020-06-05 16:43:59 +0530

    changing id

--- mysite/myapi/serializers.py
+++ mysite/myapi/serializers.py
@@ -4,16 +4,19 @@ from rest_framework import serializers
 from .models import User, ActivityPeriod
 
 class ActivityPeriodSerializer(serializers.HyperlinkedModelSerializer):
+	start_time = serializers.DateTimeField()
+	end_time = serializers.DateTimeField()
 	class Meta:
 		model = ActivityPeriod
 		fields = ('start_time', 'end_time')
 
 class UserSerializer(serializers.HyperlinkedModelSerializer):
 	activityperiods = ActivityPeriodSerializer(many=True, read_only=True)
+	id = serializers.CharField(source='uid')
 
 	class Meta:
 		model = User
-		fields = ['id', 'real_name', 'tz', 'activityperiods',]
+		fields = ['id', 'real_name', 'tz', 'activityperiods', ]
 
 class TimelineSerializer(serializers.Serializer):
     members = UserSerializer(many=True)
