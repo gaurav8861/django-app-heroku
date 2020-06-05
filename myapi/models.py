169566a8f191e06910acc05898b70af9d93f3a91
Gaurav Kumar <gkumar@cray.com>
2020-06-05 17:44:37 +0530

    sss

--- myapi/models.py
+++ myapi/models.py
@@ -1,45 +1,14 @@
 from django.db import models
 
-# Create your models here.
-# models.py
 from django.db import IntegrityError
 import string 
 import random 
-# from django.utils.crypto import get_random_string
   
 
 class User(models.Model):
-	# id = models.AutoField(primary_key=True)
-	uid = models.CharField(max_length=16, blank=True, editable=False, unique=True)
+	id = models.AutoField(primary_key=True)
 	real_name = models.CharField(max_length=60)
 	tz = models.CharField(max_length=60)
-
-	def save(self, *args, **kwargs):
-		res = ''.join(random.choices(string.ascii_uppercase +
-                             string.digits, k = 9))
-		if not self.uid:
-			self.uid = str(res)
-		success = False
-		failures = 0
-		while not success:
-			try:
-				super(User, self).save(*args, **kwargs)
-			except IntegrityError:
-				failures += 1
-				if failures > 5:
-					raise
-				else:
-					self.uid = str(res)
-			else:
-				success = True
-	# def save(self, *args, **kwargs):
-	# 	id = self.id
-	# 	if not id:
-	# 		id = get_random_string(length=9)
-	# 	while User.objects.filter(id=id).exclude(pk=self.pk).exists():
-	# 		id = get_random_string(length=9)
-	# 		self.id = id
-	# 	super(User, self).save(*args, **kwargs)
 
 class ActivityPeriod(models.Model):
 	id = models.AutoField(primary_key=True)
